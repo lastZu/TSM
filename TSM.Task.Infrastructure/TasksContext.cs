506ff9025cfb15a7a9156b9cdd253c14e5c95573
lastZu <cdrom2015@ya.ru>
2024-01-14 15:53:09 +0300

    refactoring TaskContext

--- TSM.Task.Infrastructure/TasksContext.cs
+++ TSM.Task.Infrastructure/TasksContext.cs
@@ -1,43 +1,29 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore;
+using EFCore.NamingConventions;
 
 using TSM.Task.Domain.Entities;
 
 namespace TSM.Task.Infrastructure;
-public class Context : DbContext
+public class TaskContext : DbContext
 {
 	public DbSet<Domain.Entities.Task> Tasks { get; set; }
 	public DbSet<Category> Categories { get; set; }
 	public DbSet<Priority> Priorities { get; set; }
-	private readonly string _connectionString;
 
-	public Context(string connectionString)
+	public TaskContext(DbContextOptions<TaskContext> options)
+		: base(options)
 	{
-		_connectionString = connectionString;
+		// TODO - Remove before setup Migration
 		Database.EnsureDeleted();
 		Database.EnsureCreated();
 	}
 
-	public Context(DbContextOptions options)
-		: base(options)
-	{
+    protected override void OnConfiguring(DbContextOptionsBuilder options) =>
+		options.UseSnakeCaseNamingConvention();
 
-	}
-
-	protected override void OnConfiguring(DbContextOptionsBuilder options)
-	{
-		options.UseNpgsql(
-			_connectionString
-		);
-	}
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        var taskBuilder = modelBuilder.Entity<Domain.Entities.Task>();
-		taskBuilder
-			.Property(i => i.Dedline)
-			.HasColumnType("timestamp without time zone")
-			.HasDefaultValue(DateTime.Now)
-		;
-    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+		modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(TaskContext).Assembly
+        );
 }
